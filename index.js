17b088e1f13e5e3b32dc7ca73a8062277178b530
opando <oscarpando@gmail.com>
2017-02-21 21:38:56 -0500

    correcion puerto

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 //init express
 var app = express();
 
-app.set('port',(process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -15,5 +15,5 @@ require('./route')(app);
 
 
 app.listen(app.get('port'),function(){
-  console.log('Servidor express escuchando en el puerto ' +  app.get('port'));
+  console.log('Servidor express escuchando en el puerto ' , app.get('port'));
 });

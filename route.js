8f1990282ce722e8944a969816e8442da08aa451
opando <oscarpando@gmail.com>
2017-02-22 06:09:19 -0500

    correcion token

--- route.js
+++ route.js
@@ -7,12 +7,12 @@ module.exports  = function(app){
   app.get('/webhook', function(req, res) {
 
     if (req.query['hub.mode'] === 'subscribe' &&
-        req.query['hub.verify_token'] === 'VERIFY_TOKEN') {
+        req.query['hub.verify_token'] === 'SUPER_TOKEN') {
       console.log("Validating webhook");
       res.status(200).send(req.query['hub.challenge']);
     } else {
       console.error("Failed validation. Make sure the validation tokens match.");
-      res.sendStatus(403);
+      //res.sendStatus(403);
       res.send('no configurado');
     }
   });

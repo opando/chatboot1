0a792194e25fc268b9a5f9305bdad1be847b83c7
opando <oscarpando@gmail.com>
2017-02-22 14:01:28 -0500

    implementando AI

--- route.js
+++ route.js
@@ -50,6 +50,8 @@ module.exports  = function(app,request){
 
   function receivedMessage(event) {
     var senderID = event.sender.id;
+    var sessionId = findOrCreateSession(senderID);
+
     var recipientID = event.recipient.id;
     var timeOfMessage = event.timestamp;
     var message = event.message;
@@ -59,8 +61,33 @@ module.exports  = function(app,request){
     console.log(JSON.stringify(message));
 
     var messageId = message.mid;
-
     var messageText = ' te estoy imitando -> ' + message.text;
+    clientWit.runActions(
+          sessionId, // the user's current session
+          message.text, // the user's message
+          sessions[sessionId].context // the user's current session state
+        ).then((context) => {
+          // Our bot did everything it has to do.
+          // Now it's waiting for further messages to proceed.
+          console.log('Waiting for next user messages.......');
+
+          // Based on the session state, you might want to reset the session.
+          // This depends heavily on the business logic of your bot.
+          // Example:
+          // if (context['done']) {
+          //   delete sessions[sessionId];
+          // }
+
+          // Updating the user's current session state
+          console.log("numRUc :> " + context.numRuc);
+          messageText = context.numRuc;
+          sessions[sessionId].context = context;
+        })
+        .catch((err) => {
+          console.error('Oops! Got an error from Wit: ', err.stack || err);
+        });
+
+
     var messageAttachments = message.attachments;
 
     if (messageText) {
@@ -120,5 +147,100 @@ module.exports  = function(app,request){
       }
     });
   }
+
+//----------------------------------------------------
+var WIT_TOKEN = 'GXPOPFVCFO7TZW2R36RHRYW6VUWQ3B5C';
+
+let Wit = null;
+let interactive = null;
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+  interactive = require('../').interactive;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  interactive = require('node-wit').interactive;
+}
+/*
+const accessToken = (() => {
+  if (process.argv.length !== 3) {
+    console.log('usage: node examples/quickstart.js <wit-access-token>');
+    process.exit(1);
+  }
+  return process.argv[2];
+})();
+*/
+
+// Quickstart example
+// See https://wit.ai/ar7hur/quickstart
+
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+
+const actions = {
+  send(request, response) {
+    const {sessionId, context, entities} = request;
+    const {text, quickreplies} = response;
+    console.log('sending...', JSON.stringify(response));
+  },
+  getForecast({context, entities}) {
+    var location = firstEntityValue(entities, 'location');
+    if (location) {
+      context.forecast = 'sunny in ' + location; // we should call a weather API here
+      delete context.missingLocation;
+    } else {
+      context.missingLocation = true;
+      delete context.forecast;
+    }
+    return context;
+  },
+  getNumRuc({context, entities}) {
+    var consultaRuc = firstEntityValue(entities, 'local_search_query');
+    console.log("local_search query -> " + consultaRuc);
+    console.log("context ---> " + JSON.stringify(context));
+    if (consultaRuc) {
+      context.numRuc = '1042348967'; // we should call a weather API here
+      //delete context.missingLocation;
+    } else {
+      //context.missingLocation = true;
+      //delete context.forecast;
+    }
+    return context;
+  }
+};
+
+const clientWit = new Wit({accessToken:WIT_TOKEN, actions});
+//interactive(client);
+
+
+
+const sessions = {};
+
+const findOrCreateSession = (fbid) => {
+  let sessionId;
+  // Let's see if we already have a session for the user fbid
+  Object.keys(sessions).forEach(k => {
+    if (sessions[k].fbid === fbid) {
+      // Yep, got it!
+      sessionId = k;
+    }
+  });
+  if (!sessionId) {
+    // No session found for user fbid, let's create a new one
+    sessionId = new Date().toISOString();
+    sessions[sessionId] = {fbid: fbid, context: {}};
+  }
+  return sessionId;
+};
+//----------------------------------------------------
 
 }
---
+++ service/bootWitService.js
@@ -0,0 +1,91 @@
+'use strict';
+
+let Wit = null;
+let interactive = null;
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+  interactive = require('../').interactive;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  interactive = require('node-wit').interactive;
+}
+
+const accessToken = (() => {
+  if (process.argv.length !== 3) {
+    console.log('usage: node examples/quickstart.js <wit-access-token>');
+    process.exit(1);
+  }
+  return process.argv[2];
+})();
+
+// Quickstart example
+// See https://wit.ai/ar7hur/quickstart
+
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+
+const actions = {
+  send(request, response) {
+    const {sessionId, context, entities} = request;
+    const {text, quickreplies} = response;
+    console.log('sending...', JSON.stringify(response));
+  },
+  getForecast({context, entities}) {
+    var location = firstEntityValue(entities, 'location');
+    if (location) {
+      context.forecast = 'sunny in ' + location; // we should call a weather API here
+      delete context.missingLocation;
+    } else {
+      context.missingLocation = true;
+      delete context.forecast;
+    }
+    return context;
+  },
+  getNumRuc({context, entities}) {
+    var consultaRuc = firstEntityValue(entities, 'local_search_query');
+    console.log("local_search query -> " + consultaRuc);
+    console.log("context ---> " + JSON.stringify(context));
+    if (consultaRuc) {
+      context.numRuc = '1042348967'; // we should call a weather API here
+      //delete context.missingLocation;
+    } else {
+      //context.missingLocation = true;
+      //delete context.forecast;
+    }
+    return context;
+  }
+};
+
+const client = new Wit({accessToken, actions});
+interactive(client);
+
+
+
+const sessions = {};
+
+const findOrCreateSession = (fbid) => {
+  let sessionId;
+  // Let's see if we already have a session for the user fbid
+  Object.keys(sessions).forEach(k => {
+    if (sessions[k].fbid === fbid) {
+      // Yep, got it!
+      sessionId = k;
+    }
+  });
+  if (!sessionId) {
+    // No session found for user fbid, let's create a new one
+    sessionId = new Date().toISOString();
+    sessions[sessionId] = {fbid: fbid, context: {}};
+  }
+  return sessionId;
+};

df49dcf4fda2a1b254c9ebd204c71869a696bc53
opando <oscarpando@gmail.com>
2017-02-22 23:37:12 -0500

    implementando AI

--- route.js
+++ route.js
@@ -1,6 +1,9 @@
 module.exports  = function(app,request){
-var Wit =  require('./lib/wit');
-var fetch = require('node-fetch');
+//var Wit =  require('./lib/wit');
+//var fetch = require('node-fetch');
+var PAGE_ACCESS_TOKEN = process.env.FB_PAGE_TOKEN;
+let FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;;
+
 
   app.get('/', function(req, res){
   	res.send('demo chatboot');
@@ -10,7 +13,7 @@ var fetch = require('node-fetch');
   app.get('/webhook', function(req, res) {
 
     if (req.query['hub.mode'] === 'subscribe' &&
-        req.query['hub.verify_token'] === 'SUPER_TOKEN') {
+        req.query['hub.verify_token'] === FB_VERIFY_TOKEN) {
       console.log("Validating webhook");
       res.status(200).send(req.query['hub.challenge']);
     } else {
@@ -65,30 +68,7 @@ var fetch = require('node-fetch');
 
     var messageId = message.mid;
     var messageText = ' te estoy imitando -> ' + message.text;
-    clientWit.runActions(
-          sessionId, // the user's current session
-          message.text, // the user's message
-          sessions[sessionId].context // the user's current session state
-        ).then((context) => {
-          // Our bot did everything it has to do.
-          // Now it's waiting for further messages to proceed.
-          console.log('Waiting for next user messages.......');
 
-          // Based on the session state, you might want to reset the session.
-          // This depends heavily on the business logic of your bot.
-          // Example:
-          // if (context['done']) {
-          //   delete sessions[sessionId];
-          // }
-
-          // Updating the user's current session state
-          console.log("numRUc :> " + context.numRuc);
-          messageText = context.numRuc;
-          sessions[sessionId].context = context;
-        })
-        .catch((err) => {
-          console.error('Oops! Got an error from Wit: ', err.stack || err);
-        });
 
 
     var messageAttachments = message.attachments;
@@ -112,6 +92,51 @@ var fetch = require('node-fetch');
 
   function sendGenericMessage(recipientId, messageText) {
     console.log("paso por sendGenericMessage()");
+
+    var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",
+            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/rift/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for first bubble",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your Hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",
+            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
+  };
+
+  callSendAPI(messageData);
   }
 
   function sendTextMessage(recipientId, messageText) {
@@ -127,7 +152,7 @@ var fetch = require('node-fetch');
     callSendAPI(messageData);
   }
 
-  var PAGE_ACCESS_TOKEN = "EAADZBOcZCGQYQBALEaJk7ZAJZBR9AjBd1D1rlFOZBl6w0xJnrprcoCTsJS6PfxZC7QumAIZCApE7hxoGKhBq1ZCOuNZBqPGHsDfrqvZCRDMrSpiinOsZBes9S5MsJMCtLwaS553XuTQqbMcnycKS6kxvLZCC73cDnvbntQVJxwCZBkbAjFAZDZD";
+
 
   function callSendAPI(messageData) {
     request({
@@ -152,99 +177,7 @@ var fetch = require('node-fetch');
   }
 
 //----------------------------------------------------
-var WIT_TOKEN = 'GXPOPFVCFO7TZW2R36RHRYW6VUWQ3B5C';
-/*
-let Wit = null;
-let interactive = null;
-try {
-  // if running from repo
-  Wit = require('../').Wit;
-  interactive = require('../').interactive;
-} catch (e) {
-  Wit = require('node-wit').Wit;
-  interactive = require('node-wit').interactive;
-}
-*/
-/*
-const accessToken = (() => {
-  if (process.argv.length !== 3) {
-    console.log('usage: node examples/quickstart.js <wit-access-token>');
-    process.exit(1);
-  }
-  return process.argv[2];
-})();
-*/
 
-// Quickstart example
-// See https://wit.ai/ar7hur/quickstart
-
-const firstEntityValue = (entities, entity) => {
-  const val = entities && entities[entity] &&
-    Array.isArray(entities[entity]) &&
-    entities[entity].length > 0 &&
-    entities[entity][0].value
-  ;
-  if (!val) {
-    return null;
-  }
-  return typeof val === 'object' ? val.value : val;
-};
-
-const actions = {
-  send(request, response) {
-    const {sessionId, context, entities} = request;
-    const {text, quickreplies} = response;
-    console.log('sending...', JSON.stringify(response));
-  },
-  getForecast({context, entities}) {
-    var location = firstEntityValue(entities, 'location');
-    if (location) {
-      context.forecast = 'sunny in ' + location; // we should call a weather API here
-      delete context.missingLocation;
-    } else {
-      context.missingLocation = true;
-      delete context.forecast;
-    }
-    return context;
-  },
-  getNumRuc({context, entities}) {
-    var consultaRuc = firstEntityValue(entities, 'local_search_query');
-    console.log("local_search query -> " + consultaRuc);
-    console.log("context ---> " + JSON.stringify(context));
-    if (consultaRuc) {
-      context.numRuc = '1042348967'; // we should call a weather API here
-      //delete context.missingLocation;
-    } else {
-      //context.missingLocation = true;
-      //delete context.forecast;
-    }
-    return context;
-  }
-};
-
-const clientWit = new Wit({accessToken:WIT_TOKEN, actions});
-//interactive(client);
-
-
-
-const sessions = {};
-
-const findOrCreateSession = (fbid) => {
-  let sessionId;
-  // Let's see if we already have a session for the user fbid
-  Object.keys(sessions).forEach(k => {
-    if (sessions[k].fbid === fbid) {
-      // Yep, got it!
-      sessionId = k;
-    }
-  });
-  if (!sessionId) {
-    // No session found for user fbid, let's create a new one
-    sessionId = new Date().toISOString();
-    sessions[sessionId] = {fbid: fbid, context: {}};
-  }
-  return sessionId;
-};
 //----------------------------------------------------
 
 }
